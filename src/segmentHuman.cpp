33ec8e6df5da7314ed1a9e114dc92bd286c27c4f
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-13 14:52:37 -0700

    Stub segment human

---
+++ src/segmentHuman.cpp
@@ -0,0 +1,14 @@
+/**
+ * segmentHuman.cpp
+ * segments a human from an image.
+ * returns a binary mask.
+ *
+ * Copyright (C) 2016 Alyssa Rosenzweig
+ * ALL RIGHTS RESERVED
+ */
+
+#include <opencv2/opencv.hpp>
+
+cv::Mat segmentHuman(cv::Mat& human) {
+    return human;
+}
