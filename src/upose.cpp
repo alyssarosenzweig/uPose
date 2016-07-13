c01a7e0b19e738b641a4969ae7e4d6267afade85
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-13 14:56:32 -0700

    Simplify build system

--- src/upose.cpp
+++ src/upose.cpp
@@ -1,13 +1,17 @@
 /**
- * segmentHuman.cpp
- * segments a human from an image.
- * returns a binary mask.
+ * upose.cpp
+ * the uPose pose estimation library
  *
  * Copyright (C) 2016 Alyssa Rosenzweig
  * ALL RIGHTS RESERVED
  */
 
 #include <opencv2/opencv.hpp>
+
+/**
+ * segments a human from an image
+ * returns a binary mask
+ */
 
 cv::Mat segmentHuman(cv::Mat& human) {
     return human;
