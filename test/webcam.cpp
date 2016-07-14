45e109d22a4b52149e4c7b6c5857308ca7b59926
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-14 13:49:17 -0700

    Webcam may be easier

---
+++ test/webcam.cpp
@@ -0,0 +1,25 @@
+/**
+ * webcam.c
+ * Pose estimation from the webcam.
+ * This file is part of uPose.
+ *
+ * Copyright (C) 2016 Alyssa Rosenzweig
+ * ALL RIGHTS RESERVED
+ *
+ * Usage:
+ * $ ./test/webcam
+ */
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv) {
+    cv::VideoCapture camera(0);
+
+    upose::Context context(camera);
+
+    for(;;) {
+        (void) context.step();
+
+        if(cv::waitKey(30) == 27) break;
+    }
+}
