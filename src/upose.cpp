606fa876a8e8a16e729750ab86f42a21a9f9bc56
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-17 20:23:21 -0700

    Maybe try to segment skin? ugh

--- src/upose.cpp
+++ src/upose.cpp
@@ -31,16 +31,35 @@ namespace upose {
         cv::threshold(foreground, foreground, 0.1, 255, cv::THRESH_BINARY);
         cv::cvtColor(foreground, foreground, CV_GRAY2BGR);
 
-        return foreground & frame;
+        return foreground;
     }
 
     void Context::step() {
         cv::Mat frame;
         m_camera.read(frame);
 
-        cv::Mat foreground = backgroundSubtract(frame) & frame;
+        cv::Mat foreground = backgroundSubtract(frame);
+
+        /* segment skin */
+        /* I really want to replace this for so many reasons
+         * notably, it assumes the skin color of the user (light in my case)
+         * TODO: find a better algorithm
+         */
+
+        cv::Mat channels[3];
+        cv::split(frame, channels);
+        cv::Mat skinSpace = cv::abs(channels[1] - channels[2]);
+
+        cv::Mat lowerSkin, upperSkin;
+        cv::threshold(skinSpace, lowerSkin, 4, 255, cv::THRESH_BINARY);
+        cv::threshold(skinSpace, upperSkin, 14, 255, cv::THRESH_BINARY_INV);
         
-        cv::imshow("You!", foreground);
+        cv::Mat skin;
+        cv::blur(lowerSkin & upperSkin, skin, cv::Size(5, 5));
+        cv::threshold(skin, skin, 1, 255, cv::THRESH_BINARY);
+        cv::cvtColor(skin, skin, CV_GRAY2BGR);
+
+        cv::imshow("M", foreground & skin);
     }
 
     void Skeleton::visualize(cv::Mat image) {
